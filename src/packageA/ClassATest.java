47dfef0254d4fcfa5ce72f08444152f765349cf7
gaya3m <mgayathricse91@gmail.com>
2021-07-17 06:13:56 +0000

    junit

--- src/packageA/ClassATest.java
+++ src/packageA/ClassATest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class ClassATest {
-    
-    @Tag("Sanity")
-    @DisplayName("First Junit in Class A")
-    @Test
-    public void Test1() {
-        
-        System.out.println("Inside Test1");
-    }
-}
 
+	
+	@Tag("Sanity")
+	@DisplayName("First Junit in Class A")
+	@Test
+	public void Test1() {
+		
+		System.out.println("Inside Test1");
+	}
+}
